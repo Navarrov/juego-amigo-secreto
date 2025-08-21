a4f26f9692bb1b89360242600cf923ad2eba95d9
Navarrov <renenavarrov@gmail.com>
2025-08-20 23:44:03 -0400

    Agrega primeras variables y funciones

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Agregando Variables
+let listaAmigos = [];
+let inputListaAmigos = document.getElementById("amigo");
+let listaNombres = document.getElementById("listaAmigos");
+
+//Agregando Funciones
+function agregarAmigo() {
+    listaAmigos.push(inputListaAmigos.value);
+};
